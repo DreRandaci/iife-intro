62207b412ac478713d850b45b890d72e1ad34c15
Dre mf Randaci <drerandaci@gmail.com>
2017-09-05 20:00:52 -0500

    asdlkfja

--- main.js
+++ main.js
@@ -1,40 +1,7 @@
-// let Something = (function(oldSomething){
-// 	const animals = ['cat', 'dog'];
-
-// 	oldSomething.niceFunction = function() {
-// 		console.log('Im a nice function');
-// 	}
-
-// 	return oldSomething;
-
-// })(Something || {});
-
-//
-var SolarSystem = (function(oldSolarSystem){
-	const planets = ['mercury', 'venus', 'earth', 'mars', 'jupiter', 'saturn', 'uranus', 'neptune'];
-	const spacecrafts = ['Pod', 'Death Star', 'Star Destroyer', 'Hoverboard']
-
-	oldSolarSystem.getPlanets = function(){
-		return planets;
-	};
-
-	oldSolarSystem.setPlanet = function(newPlanet){
-		planets.push(newPlanet);
-	};
-
-	oldSolarSystem.getSpacecraft = function(){
-		return spacecrafts;
-	};
-
-	oldSolarSystem.setSpacecraft = function(newCraft){
-		spacecrafts.push(newCraft);
-	};
-
-
-	return oldSolarSystem;
-})(SolarSystem || {});
-
+SolarSystem.setPlanet('Planet x');
 
 let myPlanets = SolarSystem.getPlanets();
 console.log('my planets', myPlanets);
 
+let mySpacecraft = SolarSystem.getSpacecraft();
+console.log('mySpacecraft:', mySpacecraft);
