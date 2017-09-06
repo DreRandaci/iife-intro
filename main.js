c5ecc473f419a6921f69d93b2e7bfa8d7bf8b2a8
Dre mf Randaci <drerandaci@gmail.com>
2017-09-05 19:19:42 -0500

    added basic iifes

--- main.js
+++ main.js
@@ -1,1 +1,40 @@
-console.log('linked and dtf')
+// let Something = (function(oldSomething){
+// 	const animals = ['cat', 'dog'];
+
+// 	oldSomething.niceFunction = function() {
+// 		console.log('Im a nice function');
+// 	}
+
+// 	return oldSomething;
+
+// })(Something || {});
+
+//
+var SolarSystem = (function(oldSolarSystem){
+	const planets = ['mercury', 'venus', 'earth', 'mars', 'jupiter', 'saturn', 'uranus', 'neptune'];
+	const spacecrafts = ['Pod', 'Death Star', 'Star Destroyer', 'Hoverboard']
+
+	oldSolarSystem.getPlanets = function(){
+		return planets;
+	};
+
+	oldSolarSystem.setPlanet = function(newPlanet){
+		planets.push(newPlanet);
+	};
+
+	oldSolarSystem.getSpacecraft = function(){
+		return spacecrafts;
+	};
+
+	oldSolarSystem.setSpacecraft = function(newCraft){
+		spacecrafts.push(newCraft);
+	};
+
+
+	return oldSolarSystem;
+})(SolarSystem || {});
+
+
+let myPlanets = SolarSystem.getPlanets();
+console.log('my planets', myPlanets);
+
